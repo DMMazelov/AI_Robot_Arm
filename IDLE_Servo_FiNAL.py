a916cc87ed62f6593ffb6b0238dcf877133aaac3
DMMazelov <158047450+DMMazelov@users.noreply.github.com>
2026-03-04 00:59:42 +0400

    Add files via upload

---
+++ IDLE_Servo_FiNAL.py
@@ -0,0 +1,80 @@
+import cv2
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+import serial
+import time
+arduino = serial.Serial('COM3', 9600)  
+time.sleep(2)
+model_path = "hand_landmarker.task"
+HAND_CONNECTIONS = [
+    (0,1),(1,2),(2,3),(3,4),
+    (0,5),(5,6),(6,7),(7,8),
+    (0,9),(9,10),(10,11),(11,12),
+    (0,13),(13,14),(14,15),(15,16),
+    (0,17),(17,18),(18,19),(19,20),
+    (5,9),(9,13),(13,17)
+]
+def to_pixel(x, y, w, h):
+    x = min(max(x, 0.0), 1.0)
+    y = min(max(y, 0.0), 1.0)
+    return int(x * w), int(y * h)
+
+def draw_hand(image, hand_landmarks):
+    h, w, _ = image.shape
+    pts = [to_pixel(lm.x, lm.y, w, h) for lm in hand_landmarks]
+
+    for a, b in HAND_CONNECTIONS:
+        cv2.line(image, pts[a], pts[b], (0,255,0), 2)
+    for x, y in pts:
+        cv2.circle(image, (x, y), 4, (0,0,255), -1)
+    return pts
+def is_thumb_up(pts):
+    return 1 if pts[4][0] < pts[3][0] else 0
+def is_index_up(pts):
+    return 1 if pts[8][1] < pts[6][1] else 0
+def is_middle_up(pts):
+    return 1 if pts[12][1] < pts[10][1] else 0
+def is_ring_up(pts):
+    return 1 if pts[16][1] < pts[14][1] else 0
+def is_pinky_up(pts):
+    return 1 if pts[20][1] < pts[18][1] else 0
+base_options = python.BaseOptions(model_asset_path=model_path)
+options = vision.HandLandmarkerOptions(
+    base_options=base_options,
+    num_hands=1,
+    min_hand_detection_confidence=0.5,
+    running_mode=vision.RunningMode.VIDEO
+)
+landmarker = vision.HandLandmarker.create_from_options(options)
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret: break
+    frame = cv2.flip(frame,1)
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb)
+    timestamp = int(time.time() * 1000)
+
+    result = landmarker.detect_for_video(mp_image, timestamp)
+
+    if result.hand_landmarks:
+        for hand_landmarks in result.hand_landmarks:
+            pts = draw_hand(frame, hand_landmarks)
+            values = [
+                is_thumb_up(pts),
+                is_index_up(pts),
+                is_middle_up(pts),
+                is_ring_up(pts),
+                is_pinky_up(pts)
+            ]
+            arduino.write(f"{','.join(map(str, values))}\n".encode())
+            cv2.putText(frame, f"Thumb:{values[0]} Index:{values[1]} Middle:{values[2]} Ring:{values[3]} Pinky:{values[4]}",
+                        (10,50), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (0,255,0), 2)
+
+    cv2.imshow("AI Robot Arm", frame)
+    if cv2.waitKey(1) & 0xFF == 27: break
+cap.release()
+cv2.destroyAllWindows()
+arduino.close()
